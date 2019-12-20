78f447eff65591790c3c44e236881a3e04928a2a
Haeeul <etoilune00@gmail.com>
2019-12-21 03:44:14 +0900

    1차과제_로그인화면 구현

---
+++ app/src/main/java/com/example/withsopt/feature/follower_list/FollowerListActivity.kt
@@ -0,0 +1,13 @@
+package com.example.withsopt.feature.follower_list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.withsopt.R
+
+class FollowerListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_follower_list)
+    }
+}
